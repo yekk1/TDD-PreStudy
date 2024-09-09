3a637a3357f25b7cbf5edcabb8d811aaf1375fa9
yekk1 <daeyoi96@gmail.com>
2024-09-09 21:00:21 +0900

    feat: 서비스 만료일 계산기

---
+++ src/test/java/org/chap03/ExpiryDateCalculator.java
@@ -0,0 +1,9 @@
+package org.chap03;
+
+import java.time.LocalDate;
+
+public class ExpiryDateCalculator {
+    public LocalDate calculateExpiryDate(LocalDate billingDate, int payAmount){
+        return billingDate.plusMonths(1);
+    }
+}
